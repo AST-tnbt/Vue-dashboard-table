d0b6624614fddd4fd981e25de24695266469a9d8
ast-tnbt <trinhtin272@gmail.com>
2024-10-20 15:16:09 +0700

    restructure with not using set up in script tag

--- src/pages/Import.vue
+++ src/pages/Import.vue
@@ -45,71 +45,77 @@
         </v-card>
     </v-dialog>
 </template>
-<script setup>
+<script>
     import Papa from 'papaparse'; 
-    import * as XLSX from 'xlsx'; 
-    import {ref, watch} from "vue";
+    import * as XLSX from 'xlsx';
 
-    const file = ref(null);
-    const items = ref([]);
-    const dialog = ref(false);
-    const loading = ref(false);
-
-    watch(file, () => {
-        if (!file.value) items.value = [];
-    })
-
-    const handleUploadFile = () => {
-        if (!file.value) {
-            return;
-        }
-
-        loading.value = true;
-        const startTime = Date.now();
-
-        const reader = new FileReader();
-        reader.onload = (e) => {
-            const data = e.target.result;
-            if (file.value.name.endsWith(".csv")) {
-                parseCSV(data);
-            } else if (file.value.name.endsWith(".xlsx")) {
-                parseXLSX(data);
+    export default {
+        data() {
+            return {
+                file: null,
+                items: [],
+                dialog: false,
+                loading: false
+            };
+        },
+        watch: {
+            file(newValue) {
+                if (!newValue) {
+                    this.items = [];
+                }
             }
-            const timeRemaining = Date.now() - startTime; 
-            if (timeRemaining < 2000) {
-                setTimeout(() => {
-                    loading.value = false;
-                }, 2000 - timeRemaining);
-            }
-            loading.value = false;
-        } 
+        },
+        methods: {
+            handleUploadFile() {
+                if (!this.file) {
+                    return;
+                }
+
+                this.loading = true;
+                const startTime = Date.now();
+
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    const data = e.target.result;
+                    if (this.file.name.endsWith(".csv")) {
+                        this.parseCSV(data);
+                    } else if (this.file.name.endsWith(".xlsx")) {
+                        this.parseXLSX(data);
+                    }
+                    const timeRemaining = Date.now() - startTime; 
+                    if (timeRemaining < 2000) {
+                        setTimeout(() => {
+                            this.loading = false;
+                        }, 2000 - timeRemaining);
+                    }
+                    this.loading = false;
+                } 
 
 
-        reader.readAsArrayBuffer(file.value);
-    }
-
-    const parseCSV = (data) => {
-        Papa.parse(new Blob([data]), {
-        complete: (result) => {
-            // console.log(result.data);
-            items.value = result.data;
+                reader.readAsArrayBuffer(this.file);
             },
-            header: true,
-        });
-    }
-
-    const parseXLSX = (data) => {
-        const workbook = XLSX.read(data, { type: 'array' });
-        const sheetName = workbook.SheetNames[0];
-        const worksheet = workbook.Sheets[sheetName];
-        const jsonData = XLSX.utils.sheet_to_json(worksheet);
-        // console.log(jsonData);
-        items.value = jsonData;
-    };
-
-    const clearData = () => {
-        items.value = [];
-        dialog.value = false;
-        file.value = null;
+            parseCSV(data) {
+                Papa.parse(new Blob([data]), {
+                complete: (result) => {
+                    // console.log(result.data);
+                    this.items = result.data;
+                    },
+                    header: true,
+                });
+            },
+            parseXLSX(data) {
+                const workbook = XLSX.read(data, { type: 'array' });
+                const sheetName = workbook.SheetNames[0];
+                const worksheet = workbook.Sheets[sheetName];
+                const jsonData = XLSX.utils.sheet_to_json(worksheet);
+                // console.log(jsonData);
+                this.items = jsonData;
+            },
+            clearData() {
+                this.items = [];
+                this.dialog = false;
+                this.file = null;
+            }
+        }
     }
 </script>
